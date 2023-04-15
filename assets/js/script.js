54fb16cc00facb1c4b02cecfff197d7494e6296d
D3lyth <114830659+D3lyth@users.noreply.github.com>
2023-04-15 22:19:07 +0100

    Add event listener to buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,21 @@
+// Wait for the DOM to finish loading before running the game
+// Get the button elements and add event listner to them
+
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
+
+    for (let button of buttons){
+        button.addEventListener("click", function(){
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert(`You clicked ${gameType}`);
+            }
+        })
+    }
+})
+
 function runGame() {
 
 }
@@ -6,7 +24,7 @@ function checkAnswer() {
 
 }
 
-function calculateCorrectAnser() {
+function calculateCorrectAnswer() {
 
 }
  function incrementScore() {
